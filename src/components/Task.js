5d10f91f4031c76f937bfb6149a81b50adfc8912
Michael Wilson <mikallwilsonn@gmail.com>
2020-04-09 14:41:22 -0400

    Improved the layout of the task component to a more tradition look

--- src/components/Task.js
+++ src/components/Task.js
@@ -21,6 +21,41 @@ export default function Task({ task, completeTask, removeTask, editTask }) {
 
     return (
       <div className={`task ${task.isCompleted ? "completed" : "" }`}>
+        <div className="task-complete-wrapper">
+            { task.isCompleted === false ?
+                <button 
+                    className="complete" 
+                    onClick={() => completeTask( task._id )}
+                >
+                    <svg 
+                        aria-hidden="true" 
+                        focusable="false" d
+                        ata-prefix="far" 
+                        data-icon="square" 
+                        className="svg-inline--fa fa-square fa-w-14" 
+                        role="img" 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 448 512"
+                    >
+                        <path 
+                            fill="currentColor" 
+                            d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h340c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6z"
+                        ></path>
+                    </svg>
+                </button>
+                :
+                <svg 
+                    version="1.1" 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="512" 
+                    height="512" 
+                    viewBox="0 0 512 512"
+                >
+                    <path d="M432 64l-240 240-112-112-80 80 192 192 320-320z"></path>
+                </svg>
+            }
+        </div>
+
         <div 
             className="task-label font-semi-bold"
             onClick={() => setIsEditing( true )}
@@ -42,25 +77,6 @@ export default function Task({ task, completeTask, removeTask, editTask }) {
         </div>
 
         <div className="task-actions">
-            { task.isCompleted === false ?
-                <button 
-                    className="complete" 
-                    onClick={() => completeTask( task._id )}
-                >
-                    <svg 
-                        version="1.1" 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        width="512" 
-                        height="512" 
-                        viewBox="0 0 512 512"
-                    >
-                        <path d="M432 64l-240 240-112-112-80 80 192 192 320-320z"></path>
-                    </svg>
-                </button>
-                :
-                ''
-            }
-
             <button 
                 className="delete" 
                 onClick={() => removeTask( task._id )}
